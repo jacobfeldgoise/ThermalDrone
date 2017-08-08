38c6f75da7d182bf8cfd88b39be177cde5c12408
Jacob Feldgoise <jacob@feldgoise.com>
2017-08-08 10:03:42 -0400

    Added timestamp to thermal image name. This allows for the function to be looped without overwriting previous images

--- Sensors/Lepton FLiR/basic_flir_image_capture.py
+++ Sensors/Lepton FLiR/basic_flir_image_capture.py
@@ -2,6 +2,18 @@ import numpy as np
 import cv2
 from pylepton import Lepton
 
+def timestamp():
+
+    #### This function generates a timestemp which will be written to the log file ####
+
+    local_timezone = tzlocal.get_localzone()
+    utc_time = datetime.utcnow()
+
+    # Convert time zone
+    local_time= utc_time.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+    timestamp_local = local_time.strftime('%m-%d-%Y %H:%M:%S %Z')
+    return timestamp_local
+
 with Lepton("/dev/spidev0.1") as l:
   a,_ = l.capture()
 cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX) # extend contrast
@@ -16,4 +28,4 @@ M = cv2.getRotationMatrix2D(center, 180, 1.0)
 rotated_image = cv2.warpAffine(a, M, (w, h))
 cv2.waitKey(0)
 
-cv2.imwrite("output.jpg", rotated_image) # write it
+cv2.imwrite("img-" + timestamp() + ".jpg", rotated_image) # write it
